30e12cbf4316341c471b085583f3b9062af7b767
Guy Marshall <guymarshall9@gmail.com>
2022-02-06 22:21:44 +0000

    Random dice implemented

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
+import random
+
+
+def roll_die(number_of_sides):
+    return random.randint(1, number_of_sides)
+
+
 def main():
-    print("Hello, World!")
+    number_of_dice = int(input("Number of dice to roll: "))
+    number_of_sides = int(input("Number of sides per die: "))
+
+    for i in range(number_of_dice):
+        print(roll_die(number_of_sides))
 
 
 if __name__ == '__main__':
